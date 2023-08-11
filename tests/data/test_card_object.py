ef83115bb164d8cc8b7f4298b163e2d18961a6a9
broadband <iambroadband@gmail.com>
2023-08-11 15:09:03 -0700

    Rename test files. Must be unique

---
+++ tests/data/test_card_object.py
@@ -0,0 +1,10 @@
+import pytest
+
+# TODO: WRITE TESTS FOR CARD OBJECT HERE
+
+@pytest.fixture
+def temp_fixture() -> str:
+    return "some stuff"
+
+def test_temp(temp_fixture):
+    assert temp_fixture == "some stuff"
