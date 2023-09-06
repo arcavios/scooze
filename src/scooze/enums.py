60b54e7058aac5b9948dc657c643487fe4b8a02a
Jacob Ginsparg <jacobginsparg@gmail.com>
2023-09-06 11:46:02 -0400

    Enums already auto lower with auto() so matching that behavior for DbCollection

--- src/scooze/enums.py
+++ src/scooze/enums.py
@@ -354,8 +354,8 @@ class DbCollection(ExtendedEnum, StrEnum):
     Collections in the Scooze database
     """
 
-    CARDS = "cards"
-    DECKS = "decks"
+    CARDS = auto()
+    DECKS = auto()
 
 
 # endregion
