90e6238890ea4c5ab0e351a5558289283041b1e6
Ben Horkley <ben.horkley@gmail.com>
2023-09-18 19:44:38 -0400

    docstrings and convenience methods

--- src/scooze/scooze.py
+++ src/scooze/scooze.py
@@ -5,17 +5,22 @@ from typing import Any, List
 
 import scooze.api.bulkdata as bulkdata_api
 import scooze.api.card as card_api
-import scooze.database.card as db_card
 import scooze.database.mongo as mongo
-from scooze.card import Card, CardT, FullCard
-from scooze.enums import ScryfallBulkFile
+from scooze.card import CardT, FullCard
+from scooze.enums import Format, Legality, ScryfallBulkFile
 
 CONTEXT_ERROR_STR = "Scooze used outside of 'with' context"
 
 
 class Scooze(AbstractContextManager):
     """
-    # TODO(#7): docstring
+    Context manager object for doing I/O from a MongoDB.
+
+    Sample usage:
+        with Scooze[OracleCard] as s:
+            pioneer_cards = s.get_cards_by_format(Format.PIONEER)
+            woe_cards = s.get_cards_by_set("woe")
+            black_lotus = s.get_card_by_scryfall_id
     """
 
     def __init__(self, card_class: type[CardT] = FullCard):
@@ -32,44 +37,8 @@ class Scooze(AbstractContextManager):
         mongo.mongo_close()
         # TODO(#7): stop running mongod, if we started it
 
-    # region Single card lookup
-    @cache
-    def get_card_by_name(self, name: str) -> Card | None:
-        if not self.safe_context:
-            raise RuntimeError(CONTEXT_ERROR_STR)
-
-        return card_api.get_card_by(
-            property_name="name",
-            value=name,
-            card_class=self.card_class,
-        )
-
-    @cache
-    def get_card_by_id(self, card_id: str) -> Card | None:
-        if not self.safe_context:
-            raise RuntimeError(CONTEXT_ERROR_STR)
-
-        return card_api.get_card_by(
-            property_name="_id",
-            value=card_id,
-            card_class=self.card_class,
-        )
-
-    # endregion
-
-    # region Convenience methods for cards
-
-    # TODO(#7): more convenience methods
-    #   - get_cards_by_format(format)
-    #   - get_card_by_scryfall_id
-    #   - get_card_by_oracle_id
-    #   - get_card_by_name
-    #   - get_cards_by_set
-    #   - get_cards_by_format (plus legality filters?)
-
-    # endregion
-
-    # region Multiple card lookup
+    def get_card_by(self, property_name: str, value) -> CardT:
+        return card_api.get_card_by(property_name, value, self.card_class)
 
     def get_cards_by(
         self,
@@ -88,14 +57,56 @@ class Scooze(AbstractContextManager):
             page_size=page_size,
         )
 
+    # region Convenience methods for single-card lookup
+    @cache
+    def get_card_by_name(self, name: str) -> CardT:
+        if not self.safe_context:
+            raise RuntimeError(CONTEXT_ERROR_STR)
+
+        return card_api.get_card_by(
+            property_name="name",
+            value=name,
+            card_class=self.card_class,
+        )
+
+    @cache
+    def get_card_by_scryfall_id(self, scryfall_id: str) -> CardT:
+        if not self.safe_context:
+            raise RuntimeError(CONTEXT_ERROR_STR)
+        return card_api.get_card_by(
+            property_name="_id",
+            value=scryfall_id,
+            card_class=self.card_class,
+        )
+
+    @cache
+    def get_card_by_oracle_id(self, oracle_id: str) -> CardT:
+        if not self.safe_context:
+            raise RuntimeError(CONTEXT_ERROR_STR)
+        return card_api.get_card_by(
+            property_name="oracle_id",
+            value=oracle_id,
+            card_class=self.card_class,
+        )
+
     # endregion
 
+    # region Convenience methods for multiple card lookup
+
     def get_cards_by_set(self, set_name: str) -> List[CardT]:
+        if not self.safe_context:
+            raise RuntimeError(CONTEXT_ERROR_STR)
         return card_api.get_cards_by(
             property_name="set",
             values=[set_name],
             card_class=self.card_class,
         )
+
+    def get_cards_by_format(self, format: Format, legality: Legality = Legality.LEGAL) -> List[CardT]:
+        # TODO(#7)
+        raise NotImplementedError("unable to get cards by format")
+
+    # endregion
 
     # region Bulk data I/O
     def load_card_file(self, file_type: ScryfallBulkFile, bulk_file_dir: str):
