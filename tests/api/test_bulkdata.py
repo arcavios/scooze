2015ac1ce27159ebbec940e749f6d1f99012c4f3
broadband <iambroadband@gmail.com>
2023-09-21 15:23:07 -0700

    update tests.

--- tests/api/test_bulkdata.py
+++ tests/api/test_bulkdata.py
@@ -1,29 +1,35 @@
 from unittest.mock import MagicMock, patch
 
 import scooze.api.bulkdata as bulk_api
+import pytest
 from bson import ObjectId
 from scooze.card import FullCard
 from scooze.catalogs import ScryfallBulkFile
 from scooze.utils import DEFAULT_BULK_FILE_DIR
 
+@pytest.fixture(scope="module")
+def file_type() -> str:
+    return "bulk_test_cards"
+
+@pytest.fixture(scope="module")
+def bulk_file_dir() -> str:
+    return "./data/test"
+
 
 @patch("scooze.database.card.add_cards")
-def test_load_card_file(mock_add: MagicMock, cards_full: list[FullCard], capfd):
+def test_load_card_file(mock_add: MagicMock, cards_full: list[FullCard], file_type, bulk_file_dir, capfd):
     ids = [ObjectId() for _ in cards_full]
     mock_add.return_value: list[ObjectId] = ids
-    bulk_api.load_card_file(file_type="bulk_test_cards", bulk_file_dir="./data/test")
+    bulk_api.load_card_file(file_type=file_type, bulk_file_dir=bulk_file_dir)
     captured = capfd.readouterr()
     expected = f"Loading bulk_test_cards file into the database...\nLoaded {len(ids)} cards to the database.\n"
     assert captured.out == expected
 
 
 @patch("scooze.database.card.add_cards")
-def test_load_card_file_bad(mock_add: MagicMock):
+def test_load_card_file_bad(mock_add: MagicMock, file_type, bulk_file_dir, capfd):
     mock_add.return_value = None
-    # TODO: assert that bulk_api.load_card_file() prints "no cards loaded into database"
-    pass
-
-
-def test_load_card_file_error():
-    # TODO: assert that bulk_api.looad_card_file("not a real file") raies error
-    pass
+    bulk_api.load_card_file(file_type=file_type, bulk_file_dir=bulk_file_dir)
+    captured = capfd.readouterr()
+    expected = f"Loading bulk_test_cards file into the database...\nNo cards loaded into database.\n"
+    assert captured.out == expected
