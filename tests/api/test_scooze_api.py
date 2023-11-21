332bbef3dead442c0c30f9efd9738f6e867669f5
Jacob Ginsparg <jacobginsparg@gmail.com>
2023-11-20 22:20:24 -0500

    Tests for context managers with populated database

--- tests/api/test_scooze_api.py
+++ tests/api/test_scooze_api.py
@@ -1,17 +1,22 @@
 from unittest.mock import MagicMock, patch
 
 import pytest
-import scooze.api.card as card_api
+from beanie import init_beanie
 from scooze.api import AsyncScoozeApi, ScoozeApi
 from scooze.card import Card
 from scooze.catalogs import Color
+from scooze.config import CONFIG
 from scooze.models.card import CardModel, CardModelData
+from scooze.mongo import db, mongo_close, mongo_connect
 
 
 @pytest.mark.context
 class TestScoozeApiWithPopulatedDatabase:
     @pytest.fixture(scope="class", autouse=True)
     async def populate_db(self, cards_json):
+        await mongo_connect()
+        await init_beanie(database=db.client[CONFIG.mongo_db], document_models=[CardModel])
+
         for card_json in cards_json:
             card_data = CardModelData.model_validate_json(card_json)
             card = CardModel.model_validate(card_data.model_dump(mode="json", by_alias=True))
@@ -20,27 +25,54 @@ class TestScoozeApiWithPopulatedDatabase:
         yield
 
         await CardModel.delete_all()
+        await mongo_close()
 
-    # async def test_get_card_by_async(self, recall_base: Card):
-    #     async with AsyncScoozeApi(card_class=Card) as s:
-    #         card = await s.get_card_by(property_name="name", value="Ancestral Recall")
-    #         assert card == recall_base
-    #         card = await s.get_card_by(property_name="colors", value=[Color.BLUE])
-    #         assert card == recall_base
-    #         card = await s.get_card_by(property_name="produced_mana", value=[])
-    #         assert card == recall_base
-    #         card = await s.get_card_by(property_name="producedMana", value=[])
-    #         assert card == recall_base
+    @patch("scooze.api.mongo_connect")
+    @patch("scooze.api.mongo_close")
+    @patch("scooze.api.init_beanie")
+    async def test_get_card_by_async(
+        self,
+        mock_beanie: MagicMock,
+        mock_close: MagicMock,
+        mock_connect: MagicMock,
+        recall_base: Card,
+    ):
+        async with AsyncScoozeApi(card_class=Card) as s:
+            card = await s.get_card_by(property_name="name", value="Ancestral Recall")
+            recall_base.scooze_id = card.scooze_id
+            assert card == recall_base
+            card = await s.get_card_by(property_name="colors", value=[Color.BLUE])
+            assert card == recall_base
+            card = await s.get_card_by(property_name="oracle_text", value="Target player draws three cards.")
+            assert card == recall_base
+            card = await s.get_card_by(property_name="oracleText", value="Target player draws three cards.")
+            assert card == recall_base
 
-    def test_get_card_by_sync(self, recall_base: Card):
+        mock_connect.assert_called_once()
+        mock_close.assert_called_once()
+        mock_beanie.assert_called_once()
+
+    @patch("scooze.api.mongo_connect")
+    @patch("scooze.api.mongo_close")
+    @patch("scooze.api.init_beanie")
+    def test_get_card_by_sync(
+        self,
+        mock_beanie: MagicMock,
+        mock_close: MagicMock,
+        mock_connect: MagicMock,
+        recall_base: Card,
+    ):
         with ScoozeApi(card_class=Card) as s:
             card: Card = s.get_card_by(property_name="name", value="Ancestral Recall")
             recall_base.scooze_id = card.scooze_id
             assert card == recall_base
             card = s.get_card_by(property_name="colors", value=[Color.BLUE])
             assert card == recall_base
-            card = s.get_card_by(property_name="produced_mana", value=[])
-            print(card is None)
+            card = s.get_card_by(property_name="oracle_text", value="Target player draws three cards.")
             assert card == recall_base
-            card = s.get_card_by(property_name="producedMana", value=[])
+            card = s.get_card_by(property_name="oracleText", value="Target player draws three cards.")
             assert card == recall_base
+
+        mock_connect.assert_called_once()
+        mock_close.assert_called_once()
+        mock_beanie.assert_called_once()
