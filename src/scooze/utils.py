e4c507d2bf4d6ea96f0c9987788fa73029fadbf2
broadband <iambroadband@gmail.com>
2024-03-29 20:18:03 -0700

    remove unused formatter from before we were using cleo.

--- src/scooze/utils.py
+++ src/scooze/utils.py
@@ -43,14 +43,6 @@ def scooze_logger() -> logging.Logger:
     return logging.getLogger("scooze")
 
 
-class SmartFormatter(argparse.RawDescriptionHelpFormatter, argparse.HelpFormatter):
-    def _split_lines(self, text, width):
-        if text.startswith("R|"):
-            return text[2:].splitlines()
-        # this is the RawTextHelpFormatter._split_lines
-        return argparse.HelpFormatter._split_lines(self, text, width)
-
-
 # region Deck Format Helpers
 
 
