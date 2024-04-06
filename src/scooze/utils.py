a9f7853c6d43f4060c0f98b3592eedced1869957
broadband <iambroadband@gmail.com>
2024-04-06 15:10:15 -0700

    add extra to the JsonLogFormatter

--- src/scooze/utils.py
+++ src/scooze/utils.py
@@ -28,6 +28,31 @@ DATE_FORMAT = "%Y-%m-%d"
 
 # region Logging Utils
 
+LOG_RECORD_BUILTIN_ATTRS = {
+    "args",
+    "asctime",
+    "created",
+    "exc_info",
+    "exc_text",
+    "filename",
+    "funcName",
+    "levelname",
+    "levelno",
+    "lineno",
+    "module",
+    "msecs",
+    "message",
+    "msg",
+    "name",
+    "pathname",
+    "process",
+    "processName",
+    "relativeCreated",
+    "stack_info",
+    "thread",
+    "threadName",
+    "taskName",
+}
 
 class JsonLoggingFormatter(logging.Formatter):
     def __init__(self, *, fmt_keys: dict[str, str] | None = None):
@@ -59,6 +84,10 @@ class JsonLoggingFormatter(logging.Formatter):
         }
         # fmt: on
         message.update(always_fields)
+
+        for key, val in record.__dict__.items():
+            if key not in LOG_RECORD_BUILTIN_ATTRS:
+                message[key] = val
 
         return message
 
