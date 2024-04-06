71a0a8f85e578966161c4bad1a609429ba8171bd
broadband <iambroadband@gmail.com>
2024-04-06 13:57:22 -0700

    update scooze logger to have a debug flag for testing

--- src/scooze/__init__.py
+++ src/scooze/__init__.py
@@ -1,28 +1,26 @@
 import json
 import logging.config
 import pathlib
+import sys
 from logging import getLogger
 
 logger = getLogger(__name__)
-print("MY LOGGER NAME IS:")
-print(logger.name)
 
 config_file = pathlib.Path("configs/logging_config.json")
 with open(config_file) as f_in:
     logging_config = json.load(f_in)
 logging.config.dictConfig(config=logging_config)
 
-# TODO: add handler to the root logger if debug flag is active
-if DEBUG := True:
-    root = getLogger()
+# NOTE: Use this flag to test scooze library logging. Default to False.
+if DEBUG := False:
+    logger.setLevel(logging.DEBUG)
+    stdout_handler = logging.StreamHandler(stream=sys.stdout)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    stdout_handler.setFormatter(formatter)
+    logger.addHandler(stdout_handler)
 
-        # "": {
-        #     "level": "DEBUG",
-        #     "handlers": ["debug"]
-        # },
-        # "debug": {
-        #     "class": "logging.StreamHandler",
-        #     "level": "DEBUG",
-        #     "formatter": "simple",
-        #     "stream": "ext://sys.stdout"
-        # },
+    logger.debug("DEBUG ENABLED: scooze logger debug message")
+    logger.info("DEBUG ENABLED: scooze logger info message")
+    logger.warning("DEBUG ENABLED: scooze logger warning message")
+    logger.exception("DEBUG ENABLED: scooze logger exception message")
+    logger.critical("DEBUG ENABLED: scooze logger critical message")
