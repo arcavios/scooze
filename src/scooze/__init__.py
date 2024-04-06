b87c4c6903a3aedf0d7fbaed2a4eac43fb1e1d2a
broadband <iambroadband@gmail.com>
2024-04-06 14:26:33 -0700

    add todo for queuehandler on 3.12 or higher

--- src/scooze/__init__.py
+++ src/scooze/__init__.py
@@ -1,15 +1,15 @@
 import json
-import logging.config
+import logging
 import pathlib
 import sys
-from logging import getLogger
 
-logger = getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 config_file = pathlib.Path("configs/logging_config.json")
 with open(config_file) as f_in:
     logging_config = json.load(f_in)
 logging.config.dictConfig(config=logging_config)
+# TODO: Python 3.12 and higher supports QueueHandler for non-blocking logging
 
 # NOTE: Use this flag to test scooze library logging. Default to False.
 if DEBUG := False:
