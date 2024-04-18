9e0b8657d5c0d3e53c357a7c82d60cd6b7ea2d88
Ben Horkley <ben.horkley@gmail.com>
2024-04-18 14:03:50 -0400

    pathlib in loading test decks

--- src/scooze/console/commands/load/decks.py
+++ src/scooze/console/commands/load/decks.py
@@ -1,5 +1,6 @@
 import json
 import os
+from pathlib import Path
 
 import ijson
 from cleo.commands.command import Command
@@ -58,7 +59,7 @@ def load_all_decks(decks_dir: str):
 # TODO(#145): Can remove this once the command uses ScoozeApi
 def load_test_decks():
     try:
-        with open("./data/test/pioneer_decks.jsonl") as decks_file:
+        with (Path("./data") / "test" / "pioneer_decks.jsonl").open() as decks_file:
             # print("Inserting test decks into the database...")
             json_list = list(decks_file)
             decks = [DeckModel.model_validate(json.loads(deck_json)) for deck_json in json_list]
