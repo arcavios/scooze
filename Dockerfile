945693e6cc71ced77cfb4af2c9ae1a3159a7ffab
broadband <iambroadband@gmail.com>
2024-04-17 22:59:44 -0700

    trying it.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,21 @@
 # syntax=docker/dockerfile:1
 
-FROM python:3.11
-RUN pip install scooze
-CMD scooze run
+FROM debian:12-slim AS builder
+RUN apt-get update && \
+    apt-get install --no-install-suggests --no-install-recommends --yes pipx
+ENV PATH="/root/.local/bin:${PATH}"
+RUN pipx install poetry
+RUN pipx inject poetry poetry-plugin-bundle
+WORKDIR /src
+COPY . .
+RUN poetry bundle venv --python=/usr/bin/python3 --only=main /venv
+
+FROM gcr.io/distroless/python3-debian12
+COPY --from=builder /venv /venv
+ENTRYPOINT ["/venv/bin/scooze"]
+
+# TODO: how do I do this with the given entry point or do I need a diff entry point? etc
+CMD ["uvicorn", "scooze.main:app", "--host", "127.0.0.1", "--port", "8000"]
+# RUN uvicorn scooze.main:app --host 127.0.0.1 --port 8000
+
 EXPOSE 8000
--- src/scooze/console/commands/load/decks.py
+++ src/scooze/console/commands/load/decks.py
@@ -5,7 +5,6 @@ import os
 import ijson
 from cleo.commands.command import Command
 from cleo.helpers import option
-from scooze.models.deck import DeckModelIn
 from scooze.utils import DEFAULT_DECKS_DIR
 
 
@@ -37,34 +36,36 @@ class LoadDecksCommand(Command):
 
 # TODO(#145): Can remove this once the command uses ScoozeApi
 def load_all_decks(decks_dir: str):
-    files = os.listdir(decks_dir)
-    try:
-        for file_path in files:
-            with open(file_path, "r", encoding="utf8") as deck_file:
-                # print(f"Inserting {file_path.split('/')[-1]} file into the database...")
-                decks = [
-                    DeckModelIn.model_validate(deck_json)
-                    for deck_json in ijson.items(
-                        deck_file,
-                        "item",
-                    )
-                ]
-                # asyncio.run(deck_db.add_decks(decks))
-        print("This doesn't actually do anything yet.")
-    except OSError as e:
-        print(f"Encountered an error while trying to load {file_path.split('/')[-1]}")
-        raise e
+    pass
+#     files = os.listdir(decks_dir)
+#     try:
+#         for file_path in files:
+#             with open(file_path, "r", encoding="utf8") as deck_file:
+#                 # print(f"Inserting {file_path.split('/')[-1]} file into the database...")
+#                 decks = [
+#                     DeckModelIn.model_validate(deck_json)
+#                     for deck_json in ijson.items(
+#                         deck_file,
+#                         "item",
+#                     )
+#                 ]
+#                 # asyncio.run(deck_db.add_decks(decks))
+#         print("This doesn't actually do anything yet.")
+#     except OSError as e:
+#         print(f"Encountered an error while trying to load {file_path.split('/')[-1]}")
+#         raise e
 
 
 # TODO(#145): Can remove this once the command uses ScoozeApi
 def load_test_decks():
-    try:
-        with open("./data/test/pioneer_decks.jsonl") as decks_file:
-            # print("Inserting test decks into the database...")
-            json_list = list(decks_file)
-            decks = [DeckModelIn.model_validate(json.loads(deck_json)) for deck_json in json_list]
-            # asyncio.run(deck_db.add_decks(decks))  # TODO(#7): this need async for now, replace with Python API
-        print("This doesn't actually do anything yet.")
-    except OSError as e:
-        print("Encountered an error while trying to load test decks")
-        raise e
+    pass
+#     try:
+#         with open("./data/test/pioneer_decks.jsonl") as decks_file:
+#             # print("Inserting test decks into the database...")
+#             json_list = list(decks_file)
+#             decks = [DeckModelIn.model_validate(json.loads(deck_json)) for deck_json in json_list]
+#             # asyncio.run(deck_db.add_decks(decks))  # TODO(#7): this need async for now, replace with Python API
+#         print("This doesn't actually do anything yet.")
+#     except OSError as e:
+#         print("Encountered an error while trying to load test decks")
+#         raise e
