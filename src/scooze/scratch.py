e9ec5e483609eb70dcc8c6213a679fb8a1941f5b
broadband <iambroadband@gmail.com>
2024-05-01 23:41:29 -0700

    update scratch tests

--- src/scooze/scratch.py
+++ src/scooze/scratch.py
@@ -1,18 +1,17 @@
 # TODO: delete this file
 
-from scooze import Card
+from scooze import Card, Deck, Color, Game
+from scooze.catalogs import Format
 from scooze.logger import logger
 
 card = Card("Hello World")
 print(card)
 logger.error("Error Test")
 
-from scooze import Color, Game
 
 print(Game.ARENA)
 print(Color.BLACK)
-
-from scooze import Deck
+print(Format.LIMITED)
 
 deck = Deck(archetype="Test Deck", main={card: 4})
 print(deck.export())
