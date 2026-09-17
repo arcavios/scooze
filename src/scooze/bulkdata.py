56804c97b48235de2cd97e2b655a805ba4299dfc
Ben Horkley <ben.horkley@gmail.com>
2026-09-16 21:20:56 -0400

    update key name for download URIs

--- src/scooze/bulkdata.py
+++ src/scooze/bulkdata.py
@@ -33,7 +33,7 @@ def download_bulk_data_file(
     with requests.get(uri, stream=True, headers=SCRYFALL_API_HEADERS) as r:
         r.raise_for_status()
         bulk_file_dir.mkdir(parents=True, exist_ok=True)
-        file = bulk_file_dir / f"{bulk_file_type}.json"
+        file = bulk_file_dir / f"{bulk_file_type}.jsonl"
         with file.open(mode="wb") as f:
             for chunk in r.iter_content(chunk_size=None):
                 f.write(chunk)
@@ -61,7 +61,7 @@ def download_bulk_data_file_by_type(
     with requests.get(SCRYFALL_BULK_INFO_ENDPOINT, headers=SCRYFALL_API_HEADERS) as bulk_metadata_request:
         bulk_metadata_request.raise_for_status()
         bulk_metadata = bulk_metadata_request.json()["data"]
-    bulk_files = {t["type"]: t["download_uri"] for t in bulk_metadata}
+    bulk_files = {t["type"]: t["jsonl_download_uri"] for t in bulk_metadata}
     if bulk_file_type not in bulk_files:
         return
     download_bulk_data_file(bulk_files[bulk_file_type], bulk_file_type, bulk_file_dir)
@@ -84,7 +84,7 @@ def download_all_bulk_data_files(
     with requests.get(SCRYFALL_BULK_INFO_ENDPOINT, headers=SCRYFALL_API_HEADERS) as bulk_metadata_request:
         bulk_metadata_request.raise_for_status()
         bulk_metadata = bulk_metadata_request.json()["data"]
-    bulk_files = {t["type"]: t["download_uri"] for t in bulk_metadata}
+    bulk_files = {t["type"]: t["jsonl_download_uri"] for t in bulk_metadata}
 
     for bulk_type in ScryfallBulkFile.list():
         bulk_filename = bulk_files[bulk_type]
